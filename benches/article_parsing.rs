a31fe83d916d1448319ad1f7f6ffee22629ab567
Dimitrii Lopanov <sexykdi@gmail.com>
2025-11-18 19:54:19 +0300

    do not create hashmap for tz everytime

--- benches/article_parsing.rs
+++ benches/article_parsing.rs
@@ -1,6 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use extrablatt_v2::{Article, DefaultExtractor, Extractor};
 use select::document::Document;
+use url::Url;
 
 static HTML: &str = include_str!("../scripts/file.html");
 const URL: &str = "https://ya62.ru/text/entertainment/2025/11/15/76123751/";
@@ -16,6 +17,7 @@ fn bench_full_article_parsing(c: &mut Criterion) {
 fn bench_individual_extractors(c: &mut Criterion) {
     let doc = Document::from(HTML);
     let extractor = DefaultExtractor::default();
+    let base_url = Url::parse(URL).ok();
 
     c.bench_function("extract_title", |b| {
         b.iter(|| {
@@ -37,13 +39,44 @@ fn bench_individual_extractors(c: &mut Criterion) {
 
     c.bench_function("extract_publishing_date", |b| {
         b.iter(|| {
-            extractor.publishing_date(black_box(&doc), None)
+            extractor.publishing_date(black_box(&doc), base_url.as_ref())
         });
     });
 
     c.bench_function("extract_meta_language", |b| {
         b.iter(|| {
             extractor.meta_language(black_box(&doc))
+        });
+    });
+
+    // Missing extractors that weren't benchmarked before
+    c.bench_function("extract_meta_keywords", |b| {
+        b.iter(|| {
+            extractor.meta_keywords(black_box(&doc))
+        });
+    });
+
+    c.bench_function("extract_meta_description", |b| {
+        b.iter(|| {
+            extractor.meta_description(black_box(&doc))
+        });
+    });
+
+    c.bench_function("extract_meta_thumbnail", |b| {
+        b.iter(|| {
+            extractor.meta_thumbnail_url(black_box(&doc), base_url.as_ref())
+        });
+    });
+
+    c.bench_function("extract_meta_img", |b| {
+        b.iter(|| {
+            extractor.meta_img_url(black_box(&doc), base_url.as_ref())
+        });
+    });
+
+    c.bench_function("extract_videos", |b| {
+        b.iter(|| {
+            extractor.videos(black_box(&doc), Some(extrablatt_v2::Language::Russian))
         });
     });
 }
